9162ce29293c64a1064494349e7885eec179cfa0
Ihor <kanun41k41@gmail.com>
2020-04-14 16:39:53 +0300

    contaniers/Auth.js refactoring to redux

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers, createStore } from 'redux'
 import quizReducer from './quiz'
 import createReduser from './create'
+import authReduser from './auth'
 
 export default combineReducers({
     quiz: quizReducer,
-    create: createReduser
+    create: createReduser,
+    auth: authReduser
 })
